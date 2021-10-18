384bf597e78a9fdacf18ce72127decedc6f3e024
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-10-18 23:20:59 +0300

    Halls fetching added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useHttp } from "../Hooks/http.hook";
+
 import Header from '../Components/Header'
 import ManageHalls from '../Components/HomeSections/ManageHalls'
 import ConfigHalls from '../Components/HomeSections/ConfigHalls'
@@ -7,10 +9,23 @@ import Sessions from "../Components/HomeSections/Sessions";
 import Sales from "../Components/HomeSections/Sales";
 
 const HomePage = () => {
+
+    const { loading, request, error } = useHttp()
+
+    const getHallsHandler = async () => {
+        try {
+            const data = await request('/halls', 'GET')
+            console.log('Halls: ', JSON.parse(data.halls))
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
         <div>
             <Header/>
             <main className="conf-steps">
+                <button onClick={getHallsHandler}/>
                 <ManageHalls/>
                 <ConfigHalls/>
                 <Prices/>
